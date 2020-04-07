ccb6b4c3bb0e412655369fad89eba5e1105c8563
Vladimir Avstirychenko <liquid-05@mail.ru>
2020-04-07 20:26:06 +0300

    Initial Commit

---
+++ My1COrders/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  My1COrders
+//
+//  Created by Владимир on 07.04.2020.
+//  Copyright © 2020 VladCorp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ My1COrders/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  My1COrders
+//
+//  Created by Владимир on 07.04.2020.
+//  Copyright © 2020 VladCorp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
